d730cccde66fe414b33f3ee8c1d7d4b1171dbd67
Mehmood <halcyoona@gmail.com>
2018-05-31 23:16:56 +0500

    g

---
+++ 11-php/conditional.php
@@ -0,0 +1,57 @@
+<?php
+
+#conditonals
+/*
+	==
+	===
+	<
+	>
+	<=
+	>=
+	!=
+	!==
+*/
+/*
+$num = "5";
+if ($num == 5){
+	echo"five passed<br>";
+}
+
+$num = "5";
+if ($num === 5){
+	echo"five passed<br>";
+}
+elseif($num == 6){
+	echo "six Passed";
+}
+else{
+	echo "did not passed <br>";
+}
+*/
+
+
+#Nested if you know
+
+#logical operators
+/*
+	add &&
+	or ||
+	xor 
+*/
+
+#switch
+
+$color = "green";
+
+switch($color){
+	case "red":
+		echo "My fav color";
+		break;
+	case "black":
+		echo "Your fav color";
+		break;
+	default:
+		echo "not my and your fav color";
+}
+
+?>
