00e272a71d4514051d2791e0ebd31e140be822ef
Mehmood <halcyoona@gmail.com>
2018-05-31 23:13:46 +0500

    good

---
+++ 11-php/variables.php
@@ -0,0 +1,35 @@
+<?php
+	// single line argument
+	# single line argument
+	/*
+		multiline argument
+	*/
+
+	#variables
+	/*  prefix - $
+		start with a letter or an underscore
+		only letters number and underscores
+		Case Sensitive
+	*/
+
+
+	#Data types
+	/*
+		string
+		integers
+		floats
+		boolean
+		array
+		objects
+		Null
+		Resources
+	*/
+	$str1 = "Hello";
+	$str2 = "World!";
+	$str3 = $str1 ." ". $str2;
+	$num1 = 12;
+	$num2 = 11;
+	$num3 = $num1 + $num2;
+	$float1 = 1.2;
+	echo $str3;
+?>
