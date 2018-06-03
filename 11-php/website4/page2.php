934e649f91681dbdcccf7f19f4519ac6d58239f0
Mehmood <halcyoona@gmail.com>
2018-06-03 22:34:22 +0500

    g

---
+++ 11-php/website4/page2.php
@@ -0,0 +1,16 @@
+<?php
+	session_start(); 
+	$name = $_SESSION['name'];
+	$email = $_SESSION['email'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Sessions</title>
+</head>
+<body>
+	<h4>Thank you <?php  echo $name; ?>, You have subscribed with the email <?php echo $email; ?></h4>
+	<a href="page3.php">Go To page 3</a>
+</body>
+</html>
